21426049ac908bca5e182cef4c92dd22b6483ef6
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-07-18 13:37:33 +0530

    Create classify_images.py

---
+++ classify_images.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
+#                                                                             
+# PROGRAMMER: Pranjali Bhardwaj
+# DATE CREATED: 16/07/2024                                
+# REVISED DATE: 16/07/2024
+# PURPOSE: Your city is hosting a citywide dog show and you have volunteered to help the organizing committee with contestant registration. Every participant that registers must submit an image of their dog along with biographical information about their dog. The registration system tags the images based upon the biographical information.
+
+#Some people are planning on registering pets that aren’t actual dogs.
+
+#You need to use an already developed Python classifier to make sure the participants are dogs.
+
+#Note, you DO NOT need to create the classifier. It will be provided to you. You will need to apply the Python tools you just learned to USE the classifier.
+#
+##
+# Imports classifier function for using CNN to classify images
+from classifier import classifier 
+
+# TODO 3: Define classify_images function below, specifically replace the None
+#       below by the function definition of the classify_images function. 
+#       Notice that this function doesn't return anything because the 
+#       results_dic dictionary that is passed into the function is a mutable 
+#       data type so no return is needed.
+# 
+import os
+def classify_images(images_dir, results_dic, model):
+    """
+    Creates classifier labels with classifier function, compares pet labels to 
+    the classifier labels, and adds the classifier label and the comparison of 
+    the labels to the results dictionary using the extend function. Be sure to
+    format the classifier labels so that they will match your pet image labels.
+    The format will include putting the classifier labels in all lower case 
+    letters and strip the leading and trailing whitespace characters from them.
+    For example, the Classifier function returns = 'Maltese dog, Maltese terrier, Maltese' 
+    so the classifier label = 'maltese dog, maltese terrier, maltese'.
+    Recall that dog names from the classifier function can be a string of dog 
+    names separated by commas when a particular breed of dog has multiple dog 
+    names associated with that breed. For example, you will find pet images of
+    a 'dalmatian'(pet label) and it will match to the classifier label 
+    'dalmatian, coach dog, carriage dog' if the classifier function correctly 
+    classified the pet images of dalmatians.
+     PLEASE NOTE: This function uses the classifier() function defined in 
+     classifier.py within this function. The proper use of this function is
+     in test_classifier.py Please refer to this program prior to using the 
+     classifier() function to classify images within this function 
+     Parameters: 
+      images_dir - The (full) path to the folder of images that are to be
+                   classified by the classifier function (string)
+      results_dic - Results Dictionary with 'key' as image filename and 'value'
+                    as a List. Where the list will contain the following items: 
+                  index 0 = pet image label (string)
+                --- where index 1 & index 2 are added by this function ---
+                  NEW - index 1 = classifier label (string)
+                  NEW - index 2 = 1/0 (int)  where 1 = match between pet image
+                    and classifer labels and 0 = no match between labels
+      model - Indicates which CNN model architecture will be used by the 
+              classifier function to classify the pet images,
+              values must be either: resnet alexnet vgg (string)
+     Returns:
+           None - results_dic is mutable data type so no return needed.         
+    """
+    for key in results_dic:
+        # Call the classifier function to classify the image
+        classified = classifier(images_dir + '/' + key, model)
+        
+        # Convert the classifier result to lowercase and strip any leading/trailing whitespace
+        model_label = classified.lower().strip()
+        
+        # Get the truth label from the results dictionary
+        truth = results_dic[key][0]
+        
+        # Check if the truth label is in the classifier's result
+        if truth in model_label:
+            results_dic[key].extend((model_label, 1))
+        else:
+            results_dic[key].extend((model_label, 0))
+    
+    print(results_dic)
